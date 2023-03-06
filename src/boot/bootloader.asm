8e6c109992be7779d204bbca36c42fc6e1e2eda6
PahviMasiina <58918215+PahviMasiina@users.noreply.github.com>
2023-03-06 17:37:58 +0200

    Add files via upload

--- src/boot/bootloader.asm
+++ src/boot/bootloader.asm
@@ -1,35 +1,43 @@
+[BITS 16]
+[ORG 0x7e00]
 cli ; no interrupts anymore
 
-mov ah, 0x0E ; func = display char
+mov ah, 0x0e ; func = display char
 mov al, 'b' ; char b (from word bootloader)
 int 0x10 ; perform bios video service
 
 mov ah, 0x41 ; verify extended services
-mov bx, 0x55AA
+mov bx, 0x55aa
 mov dl, 0x80 ; 1st hard drive
 int 0x13 ; perform bios extended disk services check
 
 cmp ah, 0 ; compare error code register to zero
 je onerror ; jump if error to onerror
 
-push dword 0x00 ; push 4 empty bytes
-push dword 0x00000001 ; dap "skipped" sectors, pt2, REVERSE ORDER
-push dword 0x00000000 ; dap "skipped" sectors, pt1, REVERSE ORDER
-push dword 0x00007e00 ; dap start memory address (right after bootloader), REVERSE ORDER
-push word 0x7f ; dap copied segments (127 pcs), REVERSE ORDER, MAY NOT WORK IN SOME PHOENIX BIOSES
-push byte 0x00 ; dap reserved, REVERSE ORDER
-push byte 0x10 ; dap size (16 bytes), REVERSE ORDER
-
 mov ah, 0x42 ; read sectors
 mov dl, 0x80 ; 1st hard drive
-mov si, sp ; move stack pointer to si
+mov si, dap ; move dap to si
 int 0x13 ; perform bios disk services
 
-jmp 0x00007e00 ; jump to the kernel
+mov es, 0x00
+mov bp, 0x7e0
+mov bp, ax
+xor bp, bp ; set 0 
+mov ah, 0x13 ; func = display string
+mov al, 0x00
+mov bh, 0x00
+mov bl, 0x01
+mov cx, 0x200 ; 512
+mov dh, 0x00
+mov dl, 0x00
+int 0x10 ; perform bios video service
+
+;jmp 0x00007e00 ; jump to the kernel
+jmp main
 
 onerror:
 
-	mov ah, 0x0E ; func = display char
+	mov ah, 0x0e ; func = display char
 	mov al, 'e' ; char e (from word error)
 	int 0x10 ; perform bios video service
 
@@ -37,7 +45,17 @@ onerror:
 	
 main:
     jmp main
- 
+
+align 4
+dap:
+    .dapSize db 10h ; dap size (16 bytes)
+    .dapZero db 0 ; dap reserved
+    .dapSectors dw 0x7f ; dap copied segments (127 pcs)
+    .dapOffset dw 0 ; zero offset?
+    .dapSegment dw 0x7e0 ; dap start memory address (right after bootloader)
+    .dapStartLow dd 0x00000001 ; dap "skipped" sectors, pt1
+    .dapStartHigh dd 0x00000000 ; dap "skipped" sectors, pt2
+
 times 440-($-$$) db 0 ; fills the rest of the bootloader with zeroes
 db 0x00 ; disk signature = 1 (4 byte)
 db 0x00
--- src/kernel.asm
+++ src/kernel.asm
@@ -1,5 +1,8 @@
+[BITS 16]
+[ORG 0x7c00]
 mov ah, 0x0e ; display char
 mov al, 'k'
 int 0x10 ; print k(ernel)
 main:
 	jmp main
+db "somestring123123"
