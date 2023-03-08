66ce687576272d34f1edd8350bd81cc9fad1d2ec
PahviMasiina <58918215+PahviMasiina@users.noreply.github.com>
2023-03-08 23:07:18 +0200

    Add files via upload
    does not work

--- src/boot/bootloader.asm
+++ src/boot/bootloader.asm
@@ -1,8 +1,11 @@
 [BITS 16]
 [ORG 0x7c00]
+
+segment .code
+
 cli ; no interrupts anymore
 
-mov ah, 0x0e ; func = display char
+mov ah, 0x0e ; func = display char ; KERNEL WAS TOO BIG
 mov al, 'b' ; char b (from word bootloader)
 int 0x10 ; perform bios video service
 
@@ -11,13 +14,23 @@ mov bx, 0x55aa
 mov dl, 0x80 ; 1st hard drive
 int 0x13 ; perform bios extended disk services check
 
-cmp ah, 0x00 ; compare error code register to zero
+cmp ah, 0x00 ; compare error code register to zero ; KERNEL WAS TOO BIG
 je onError ; jump if error to onerror
 
 mov ah, 0x42 ; read sectors
 mov dl, 0x80 ; 1st hard drive
 mov si, dap ; move dap to si
 int 0x13 ; perform bios disk services
+
+mov ah, 0x00 ; func = set video mode(clear screen, reset video output)
+mov al, 0x03 ; video mode (check http://www.columbia.edu/~em36/wpdos/videomodes.txt), 80x25 text mode
+int 0x10 ; perform bios video service
+
+lgdt [gdt] ; load gdt
+
+mov eax, cr0  ; move control register to eax
+or al, 1 ; set PE (Protection Enable) bit in CR0 (Control Register 0)
+mov cr0, eax ; move eax to control register
 
 jmp 0x7e00 ; jump to the kernel
 
@@ -32,6 +45,8 @@ onError:
 main:
     jmp main
 
+segment .data ; MUST BE BEFORE THE "FILLER" PART
+
 align 4
 dap:
     .dapSize db 10h ; dap size (16 bytes)
@@ -41,6 +56,28 @@ dap:
     .dapSegment dw 0x7e0 ; dap start memory address (right after bootloader)
     .dapStartLow dd 0x00000001 ; dap "skipped" sectors, pt1
     .dapStartHigh dd 0x00000000 ; dap "skipped" sectors, pt2
+
+align 4
+gdt:
+    .nullDescriptorBase dd 0x00000000 ; null descriptor table, REQUIRED PERHAPS
+	.nullDescriptorLimit dd 0x00000000
+	.nullDescriptorAccessByte db 0x00
+	.nullDescriptorFlags db 0x00
+	
+	.kernelCodeBase dd 0x00400000 ; kernel mode code segment table
+	.kernelCodeLimit dd 0x003fffff
+	.kernelCodeAccessByte db 0x9a
+	.kernelCodeFlags db 0x0c
+	
+	.kernelDataBase dd 0x00800000 ; kernel mode data segment table
+	.kernelDataLimit dd 0x003fffff
+	.kernelDataAccessByte db 0x92
+    .kernelDataFlags db 0x00
+	
+	.taskStateBase dd 0x009b0000 ; task state segment table
+	.taskStateLimit dd 0x003fffff ; maybe bit too small
+	.taskStateAccessByte db 0x89 ; 
+    .taskStateFlags db 0x00
 
 times 440-($-$$) db 0 ; fills the rest of the bootloader with zeroes
 db 0x00 ; disk signature = 1 (4 byte)
@@ -55,3 +92,4 @@ times 16 db 0 ; bootsector 3
 times 16 db 0 ; bootsector 4
 db 0x55 ; adds "valid bootsector" identifiers
 db 0xaa
+
--- src/kernel.asm
+++ src/kernel.asm
@@ -1,71 +1,23 @@
-[BITS 16]
+[BITS 32]
 [ORG 0x7e00]
 
-call performBiosCalls ; performs screen reset, welcometext and waits for any key
-call goProtected ; go into protected mode
+segment .code
+
+call putchar
 
 main:
-
-	
-
 	jmp main
 	
 ; USEFUL FUNCTIONS
-
-performBiosCalls:
-
-	mov ah, 0x00 ; func = set video mode(clear screen, reset video output)
-	mov al, 0x02 ; video mode (check http://www.columbia.edu/~em36/wpdos/videomodes.txt)
-	int 0x10 ; perform bios video service
 	
-	mov ah, 0x13 ; func = print string
-	mov al, 0x01 ; subservice, (check http://vitaly_filatov.tripod.com/ng/asm/asm_023.20.html)
-	mov bh, 0x00 ; page number
-	mov bl, 0x02 ; attribute, color 02 = green-on-black
-	mov dh, 0x00 ; row
-	mov dl, 0x00 ; column
-	mov cx, 0x36 ; length, 55
-	mov bp, welcometext ; string
-	int 0x10 ; perform bios video service
+putchar:
 	
-	mov ah, 0x00 ; func = read char, blocking
-	int 0x16 ; perform bios keyboard service
-
-	ret ; return
-
-goProtected: ; go into protected mode, WORKS
+	;int 0x16 ; crash test
 	
-	cli ; no interrupts anymore
-	
-	lgdt [gdt] ; load gdt
-	
-	mov eax, cr0  ; move control register to eax
-	or al, 1 ; set PE (Protection Enable) bit in CR0 (Control Register 0)
-	mov cr0, eax ; move eax to control register
+	mov byte [0x000b8000], 'X' ; puts 'h' (as a byte (obv)) into first address of video memory chars
+	mov byte [0x000b8001], 0x10 ; colour (as a byte (obv))
 	
 	ret ; return
 
 section .data
-welcometext db "Welcome to TorqOS kernel, press any key to continue..." ; welcoming text, max 80 chars
 
-align 4
-gdt:
-    .nullDescriptorBase dw 0x00000000 ; null descriptor table, REQUIRED PERHAPS
-	.nullDescriptorLimit dw 0x00000000
-	.nullDescriptorAccessByte db 0x00
-	.nullDescriptorFlags db 0x00
-	
-	.kernelCodeBase dw 0x00400000 ; kernel mode code segment table
-	.kernelCodeLimit dw 0x003fffff
-	.kernelCodeAccessByte db 0x9a
-	.kernelCodeFlags db 0x0c
-	
-	.kernelDataBase dw 0x00800000 ; kernel mode data segment table
-	.kernelDataLimit dw 0x003fffff
-	.kernelDataAccessByte db 0x92
-    .kernelDataFlags db 0x00
-	
-	.taskStateBase dw 0x009b0000 ; task state segment table
-	.taskStateLimit dw 0x003fffff ; maybe bit too small
-	.taskStateAccessByte db 0x89 ; 
-    .taskStateFlags db 0x00
