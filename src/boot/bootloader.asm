39002ddf3fbba73e819b848acebad7ca0e92fa12
PahviMasiina <58918215+PahviMasiina@users.noreply.github.com>
2023-03-09 01:07:50 +0200

    goes to guru meditation if one line is uncommented ;)

--- src/boot/bootloader.asm
+++ src/boot/bootloader.asm
@@ -1,9 +1,11 @@
 [BITS 16]
 [ORG 0x7c00]
 
-segment .code
-
 cli ; no interrupts anymore
+
+mov ah, 0x00 ; func = set video mode(clear screen, reset video output)
+mov al, 0x03 ; video mode (check http://www.columbia.edu/~em36/wpdos/videomodes.txt), 80x25 text mode
+int 0x10 ; perform bios video service
 
 mov ah, 0x0e ; func = display char ; KERNEL WAS TOO BIG
 mov al, 'b' ; char b (from word bootloader)
@@ -14,17 +16,13 @@ mov bx, 0x55aa
 mov dl, 0x80 ; 1st hard drive
 int 0x13 ; perform bios extended disk services check
 
-cmp ah, 0x00 ; compare error code register to zero ; KERNEL WAS TOO BIG
-je onError ; jump if error to onerror
+cmp ah, 0x00 ; compare error code register to zero, might cause false error on some PCs
+je onError ; jump if error to onError
 
 mov ah, 0x42 ; read sectors
 mov dl, 0x80 ; 1st hard drive
 mov si, dap ; move dap to si
 int 0x13 ; perform bios disk services
-
-mov ah, 0x00 ; func = set video mode(clear screen, reset video output)
-mov al, 0x03 ; video mode (check http://www.columbia.edu/~em36/wpdos/videomodes.txt), 80x25 text mode
-int 0x10 ; perform bios video service
 
 lgdt [gdt] ; load gdt
 
@@ -45,15 +43,13 @@ onError:
 main:
     jmp main
 
-segment .data ; MUST BE BEFORE THE "FILLER" PART
-
 align 4
 dap:
     .dapSize db 10h ; dap size (16 bytes)
     .dapZero db 0 ; dap reserved
     .dapSectors dw 0x7f ; dap copied 127 segments
-    .dapOffset dw 0 ; zero offset?
-    .dapSegment dw 0x7e0 ; dap start memory address (right after bootloader)
+    .dapOffset dw 0x00 ; dap offset
+    .dapSegment dw 0x7e0 ; dap start memory address, conventional memory right after bootsector in ram, can not be 0x7e00
     .dapStartLow dd 0x00000001 ; dap "skipped" sectors, pt1
     .dapStartHigh dd 0x00000000 ; dap "skipped" sectors, pt2
 
@@ -64,22 +60,22 @@ gdt:
 	.nullDescriptorAccessByte db 0x00
 	.nullDescriptorFlags db 0x00
 	
-	.kernelCodeBase dd 0x00400000 ; kernel mode code segment table
+	.kernelCodeBase dd 0x00400000 ; kernel mode code segment table (4Mib)
 	.kernelCodeLimit dd 0x003fffff
 	.kernelCodeAccessByte db 0x9a
-	.kernelCodeFlags db 0x0c
+	.kernelCodeFlags db 0xc
 	
-	.kernelDataBase dd 0x00800000 ; kernel mode data segment table
+	.kernelDataBase dd 0x00800000 ; kernel mode data segment table (4MiB)
 	.kernelDataLimit dd 0x003fffff
 	.kernelDataAccessByte db 0x92
-    .kernelDataFlags db 0x00
+    .kernelDataFlags db 0xc
 	
-	.taskStateBase dd 0x009b0000 ; task state segment table
-	.taskStateLimit dd 0x003fffff ; maybe bit too small
-	.taskStateAccessByte db 0x89 ; 
-    .taskStateFlags db 0x00
+	;.taskStateBase dd 0x009b0000 ; task state segment table
+	;.taskStateLimit dd 0x003fffff ; maybe bit too small
+	;.taskStateAccessByte db 0x89 ; 
+    ;.taskStateFlags db 0x0c
 
-times 440-($-$$) db 0 ; fills the rest of the bootloader with zeroes
+times 440-($-$$) db 0 ; fills the rest of the bootloader with zeroes, MUST BE IN THE SAME SEGMENT AS THE CODE, OTHERWISE WONT WORK
 db 0x00 ; disk signature = 1 (4 byte)
 db 0x00
 db 0x00
--- src/kernel.asm
+++ src/kernel.asm
@@ -1,11 +1,17 @@
 [BITS 32]
 [ORG 0x7e00]
 
-segment .code
-
+;int 0x16 ; crash test
 call putchar
+;mov byte [0x000b8000], 'X' ; puts 'X' (as a byte (obv)) into first address of video memory chars
+;mov byte [0x000b8001], 0x10 ; colour (as a byte (obv))
 
 main:
+
+	;mov ah, 0x0e ; func = display char ; KERNEL WAS TOO BIG
+	;mov al, 'b' ; char b (from word bootloader)
+	;int 0x10 ; perform bios video service
+	
 	jmp main
 	
 ; USEFUL FUNCTIONS
@@ -14,10 +20,8 @@ putchar:
 	
 	;int 0x16 ; crash test
 	
-	mov byte [0x000b8000], 'X' ; puts 'h' (as a byte (obv)) into first address of video memory chars
+	mov byte [0x000b8000], 'X' ; puts 'X' (as a byte (obv)) into first address of video memory chars
 	mov byte [0x000b8001], 0x10 ; colour (as a byte (obv))
 	
 	ret ; return
-
-section .data
 
